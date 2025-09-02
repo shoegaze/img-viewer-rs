62b80a51617177a23ccbe6fa4d4cf30872803183
shoegaze <7885170+shoegaze@users.noreply.github.com>
2025-09-02 19:22:45 +0900

    refactor: extract app execution to function

--- app-main/src/main.rs
+++ app-main/src/main.rs
@@ -6,6 +6,7 @@ mod util;
 mod window;
 
 use clap::Parser;
+use winit::error::EventLoopError;
 use winit::event_loop::{ControlFlow, EventLoop};
 
 use std::error::Error;
@@ -19,24 +20,35 @@ use crate::util::path::open_image_from_path;
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse().validated()?;
 
+    // TODO: Load app settings from file
     let settings = AppSettings::default();
     let mut app = App::from_settings(settings);
 
     for path in args.paths {
-        let Err(error) = open_image_from_path(&mut app, path) else {
-            continue;
-        };
+        let open_image_result = open_image_from_path(&mut app, path);
 
-        eprintln!("Couldn't open image: '{}'", error);
+        if let Err(error) = open_image_result {
+            eprintln!("Couldn't open image: '{}'", error);
+        };
     }
 
-    // Execute app
-    let event_loop = EventLoop::new().unwrap();
+    {
+        let app_result = run_app(&mut app);
 
-    event_loop.set_control_flow(ControlFlow::Wait);
-    event_loop
-        .run_app(&mut app)
-        .expect("EventLoop error occurred");
+        if let Err(error) = app_result {
+            eprintln!("Event loop error occurred: {}", error);
+
+            return Err(Box::from(error));
+        }
+    }
 
     Ok(())
+}
+
+fn run_app(app: &mut App) -> Result<(), EventLoopError> {
+    let event_loop = EventLoop::new()?;
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    event_loop.run_app(app)
 }
